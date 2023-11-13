e77af2c70dfc1d17c8a0a3049f9b4cb1022fff32
Loc <anhlocok123sa1@gmail.com>
2023-11-13 13:34:48 +0700

    so chinh phuong

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
+bool check_SCP(int n) {
+    int i = 0;
+    while(i*i <= n){
+	if(i*i == n){
+		return true;
+	    }
+	++i;
+    }
+    return false;
+}
+
 int main() {
     int a;
     cout<<"Nhap so nguyen a: ";
     cin>>a;
+
+    int n;
+    cout<<"Nhap n: ";
+    cin>>n;
+    if(check_SCP(n)) {
+        cout<<"La so chinh phuong";
+    } else {
+        cout<<"La so hoan thien";
+    }
+
+
+    return 1;
 }
